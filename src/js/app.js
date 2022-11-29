f7eae61a7805c5e5946ab33e0a50e989028fc2bd
Franche020 <64061827+Franche020@users.noreply.github.com>
2022-11-29 10:05:10 +0100

    Revision Final

--- src/js/app.js
+++ src/js/app.js
@@ -1,67 +1,69 @@
-// Creo la variable del boton mostrar menú
-const botonMenu = document.querySelector('.navegacion__boton');
-const botonMostrar= document.querySelector('.navegacion__boton--mostrar');
-const botonCerrar = document.querySelector('.navegacion__boton--cerrar');
-const barraNav = document.querySelector('.navegacion__enlaces');
+// Variables del boton, icono mostrar e icono cerrar ademas de la barra de navegación
+const botonMenu = document.querySelector(".navegacion__boton");
+const botonMostrar = document.querySelector(".navegacion__boton--mostrar");
+const botonCerrar = document.querySelector(".navegacion__boton--cerrar");
+const barraNav = document.querySelector(".navegacion__enlaces");
 
-
+// Variables e inicializacion tanto de si el menu está mostrado como del viewport del dispositivo
 let menuMostrado = false;
 let anchoCliente = window.visualViewport.width;
 
-
+// Inicializo la app
 app();
 
-function app () {
-    initEstado();
-    mostrarMenu();
+function app() {
+  initEstado();
+  mostrarMenu();
 }
 
+// Declaro el estado inicial para en función del tamaño del viewport mostrar o no la navbar o el boton
 function initEstado() {
+  if (anchoCliente < 768) {
+    barraGrande("none", "flex", "none");
+  } else {
+    barraGrande("flex", "none", "none");
+  }
+}
+
+// Funcion para mostrar u ocultar el menú
+function mostrarMenu() {
+  // Evento para el pulsado del boton
+  botonMenu.addEventListener("click", function () {
     if (anchoCliente < 768) {
-        barraGrande('none','flex','none');
-    } else {
-        barraGrande('flex','none','none');
+      if (menuMostrado === false) {
+        barraGrande("flex", "none", "flex");
+        menuMostrado = true;
+
+        //console.log('Mostrando el menu')
+      } else {
+        barraGrande("none", "flex", "none");
+        menuMostrado = false;
+
+        //console.log('ocultando el menu')
+      }
     }
+  });
+
+  // Evento para el cambio de tamaño de la pantalla, para que funcione correctamente se tiene que vincular al elemento window
+  window.onresize = function () {
+    anchoCliente = window.visualViewport.width;
+    if (anchoCliente > 768) {
+      barraGrande("flex", "none", "none");
+      menuMostrado = false;
+
+      //console.log('la barra se va a mostrar dentro de resize')
+    } else {
+      barraGrande("none", "flex", "none");
+      menuMostrado = false;
+
+      //console.log('la barra se va a ocultar al crecer la pantalla')
+    }
+  };
 }
 
-
-function mostrarMenu () {
-    botonMenu.addEventListener('click', function () {
-        if (anchoCliente < 768 ) {
-            if(menuMostrado===false){
-                barraGrande('flex','none','flex')
-    
-                menuMostrado = true;
-
-                //console.log('Mostrando el menu')
-            }else {
-                barraGrande('none','flex','none')
-                
-                menuMostrado = false;
-                
-                //console.log('ocultando el menu')
-            }     
-        }       
-    })
-    window.onresize = function () {
-        anchoCliente = window.visualViewport.width;
-        if (anchoCliente > 768) {
-            barraGrande('flex','none','none');
-            //console.log('la barra se va a mostrar dentro de resize')
-            menuMostrado = false;
-        } else {
-            barraGrande('none','flex','none');
-            //console.log('la barra se va a ocultar al crecer la pantalla')
-            menuMostrado = false;
-        }
-    } 
-    
+// Funcion para los estados de los elementos y reducir el código
+function barraGrande(barra, bMostrar, bCerrar) {
+  barraNav.style.display = barra;
+  botonMostrar.style.display = bMostrar;
+  botonCerrar.style.display = bCerrar;
 }
-
-
-function barraGrande (barra,bMostrar,bCerrar) {
-    barraNav.style.display = barra;  
-    botonMostrar.style.display = bMostrar;
-    botonCerrar.style.display = bCerrar;   
-}
-
